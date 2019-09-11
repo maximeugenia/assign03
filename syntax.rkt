c714e521cb10c5895e2d79e8b0f0187a4d464f70
David Van Horn <dvanhorn@cs.umd.edu>
2019-09-10 23:12:16 -0400

    Initial commit.

---
+++ syntax.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(provide (all-defined-out))
+
+;; Any -> Boolean
+;; Is x a well-formed expression?
+(define (expr? x)
+  (match x
+    [(? symbol?) #t]
+    [(? integer? i) #t]
+    [`(add1 ,x) (expr? x)]
+    [`(sub1 ,x) (expr? x)]
+    [`(if (zero? ,x) ,y ,z)
+     (and (expr? x)
+          (expr? y)
+          (expr? z))]
+    ;; TODO
+    [_ #f]))
